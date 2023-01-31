e506decffcbb3a50b99f789aeead96a76ec020fd
Carlos Alfredo Diaz <cdiazjr4@gmail.com>
2023-01-31 19:56:59 -0300

    Update fibep_interpreter.py

--- src/fibep_interpreter.py
+++ src/fibep_interpreter.py
@@ -2,16 +2,54 @@ import json
 import time
 import os
 import sys
-import daemon
 import logging
 import logging.handlers as handlers
 import base64
+import argparse
 
 
+class Article:
+    def __init__(self,
+                 provider,
+                 item_id,
+                 country_iso,
+                 language_iso,
+                 source_id,
+                 source_name,
+                 article_publication_timestamp,
+                 article_lead,
+                 article_text,
+                 article_source_channel):
+        self.provider = provider
+        self.item_id = item_id
+        self.country = country_iso
+        self.language = language_iso
+        self.source_id = source_id
+        self.source_name = source_name
+        self.article_publication_timestamp = article_publication_timestamp
+        self.article_lead = article_lead
+        self.article_text = article_text
+        self.article_source_channel=article_source_channel
+        self.additional_content = []
+        self.content_url = ""
+        self.contract_id=""
+        self.customer_id=""
+        self.profile_id=""
+        self.article_author = ""
+        self.article_headline=""
+        self.matching_words={}
+    def additional_content_decode(self,additional_content):
+        b64 = additional_content["Base64data"]
+        content=base64_decode(b64)
+        return content
+    def additional_content_encode(self,file_location, file_name):
+        attachment = load_attachment(file_location,file_name)
+        encoded_b64 = base64_encode(attachment)
+        self.additional_content["FileName"] = file_name
+        self.additional_content["Base64data"] = encoded_b64
 def base64_decode(b64):
     decoded_b64 = base64.b64decode(b64, validate=True)
     return decoded_b64
-
 def save_attachment(decoded_b64,file_location,file_name ):
     decoded_b64= decoded_b64.encode('utf-8')
     with open(file_name , 'wb') as file_to_save:
@@ -24,67 +62,53 @@ def load_attachment(file_location,file_name):
 def base64_encode(attachment):
     encoded_b64 = base64.b64encode(attachment)
     return encoded_b64
-
-class Article:
-    def __init__(self,provider,item_id,country_iso,language_iso,source_id,source_name,publish_timestamp,article_lead,article_text):
-        self.provider = provider
-        self.item_id = item_id
-        self.country = country_iso
-        self.language = language_iso
-        self.source_id = source_id
-        self.source_name = source_name
-        self.publish_timestamp = publish_timestamp
-        self.article_lead = article_lead
-        self.article_text = article_text
-        self.additional_content = []
-
-    def additional_content_decode(self,additional_content):
-        b64 = additional_content["Base64data"]
-        content=base64_decode(b64)
-        return content
-    def additional_content_encode(self,file_location, file_name):
-        attachment = load_attachment(file_location,file_name)
-        encoded_b64 = base64_encode(attachment)
-        self.additional_content["FileName"] = file_name
-        self.additional_content["Base64data"] = encoded_b64
-
-        
-        
-
-
-            
-
-
-
-
-
 def fibep_decode(encoded_json):
+    encoded_json = open(encoded_json,encoding="utf8")
     try:
+        parsed_encoded_json=json.load(encoded_json)
+        news = Article(parsed_encoded_json["Provider"],
+                       parsed_encoded_json["ItemID"],
+                       parsed_encoded_json["Country"],
+                       parsed_encoded_json["Language"],
+                       parsed_encoded_json["SourceID"],
+                       parsed_encoded_json["SourceName"],
+                       parsed_encoded_json["PublishTime"],
+                       parsed_encoded_json["Lead"],
+                       parsed_encoded_json["Text"],
+                       parsed_encoded_json["SourceChannel"])
 
-        json.loads(encoded_json)
-        news = Article(encoded_json["Provider"],
-                       encoded_json["ItemID"],
-                       encoded_json["Country"],
-                       encoded_json["Language"],
-                       encoded_json["SourceID"],
-                       encoded_json["SourceName"],
-                       encoded_json["PublishTime"],
-                       encoded_json["Headline"],
-                       encoded_json["Lead"],
-                       encoded_json["Text"],
-                       encoded_json["SourceChannel"],
-                       encoded_json["ContentURL"])
-
+        if "ContentURL" in parsed_encoded_json:
+            if parsed_encoded_json["ContentURL"]:
+                news.content_url = parsed_encoded_json["ContentURL"]
+                print("gotcha")
+        print("gotcha")
+        if "AdditionalContent" in parsed_encoded_json:
+            if parsed_encoded_json["AdditionalContent"]:
+                news.additional_content = parsed_encoded_json["AdditionalContent"]
+        if parsed_encoded_json["ContentURL"]:
+            news.content_url = parsed_encoded_json["ContentURL"] 
+        if parsed_encoded_json["ContractID"]:
+            news.contract_id=parsed_encoded_json["ContractID"] 
+        if parsed_encoded_json["CustomerID"]:
+            news.customer_id=parsed_encoded_json["CustomerID"]
+        if parsed_encoded_json["ProfileID"]:
+            news.profile_id=parsed_encoded_json["ProfileID"]
+        if parsed_encoded_json["Author"]:
+            news.article_author = parsed_encoded_json["Author"]
+        if parsed_encoded_json["Headline"]:
+            news.article_headline=parsed_encoded_json["Headline"]
+        if parsed_encoded_json["MatchingWords"]:
+            news.matching_words=parsed_encoded_json["MatchingWords"]
+        print(vars(news))
+        return news
     except Exception as exception:
-        logging.critical(f"There's an error accessing your json file, the error is the following: {exception}")
+        logging.critical(f"There's an error decoding your json file, the error is the following: {exception}")
         print("There's no json file in the program's folder, please check the logs.")
         sys.exit()
-
-
 def fibep_encode(news):
     try:
 
-        json.dumps(news)
+        json.dump(news)
 
 
     except Exception as exception:
@@ -92,8 +116,33 @@ def fibep_encode(news):
         print("There's no JSON file in the program's folder, please check the logs.")
         sys.exit()
 
+def main():
+    try:
+        parser = argparse.ArgumentParser(description='This is a simple python script to encode or decode files according to the FIBEP JSON open standard.')
+        parser.add_argument('dir', nargs="?", default=os.getcwd())
+        parser.add_argument('-e', type=str, help='Encode the file quoted after the e.')
+        parser.add_argument('-d', type=str, help='Decode the file quoted after the d.')
+        parser.add_argument('-DEBUG', action='store_true', help='Add Debug messages to log.')
+        args = parser.parse_args()
+        print(args.dir)
+        if getattr(args,'e') is not None:
+            decoded_json = os.path.join(args.dir, args.e)
+            logging.info("Running with -e to encode a JSON file.")
+            fibep_encode(decoded_json)
+        elif getattr(args,'d'):
+            encoded_json = os.path.join(args.dir, args.d)
+            logging.info("Running with -d to decode a JSON file.")
+            fibep_decode(encoded_json)
+        if getattr(args,'DEBUG'):
+            logging.info("Running with -DEBUG in DEBUG log mode.")
+
+    except Exception as exception:
+        logging.critical(f"There's an error accessing your JSON file, the error is the following: {exception}")
+        print("There's no JSON file in the program's folder, please check the logs.")
+        sys.exit()
 
 
 if __name__== '__main__':
         file_location = os.path.dirname(os.path.realpath(__file__))
         main()
+
