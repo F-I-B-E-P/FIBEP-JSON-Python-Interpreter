4f669f3bad4a07673b205220ba5cf1ea70c4872e
Carlos Alfredo Diaz <cdiazjr4@gmail.com>
2023-01-31 20:51:59 -0300

    First Working Version of decode methods

--- src/fibep_interpreter.py
+++ src/fibep_interpreter.py
@@ -50,11 +50,12 @@ class Article:
 def base64_decode(b64):
     decoded_b64 = base64.b64decode(b64, validate=True)
     return decoded_b64
-def save_attachment(decoded_b64,file_location,file_name ):
-    decoded_b64= decoded_b64.encode('utf-8')
-    with open(file_name , 'wb') as file_to_save:
-        decoded_image_data = base64.decodebytes(decoded_b64)
-        file_to_save.write(decoded_image_data)
+def save_attachment(decoded_b64,file_format,file_location,file_name):
+    #decoded_b64= decoded_b64.encode('utf-8')
+    file=os.path.join(file_location, file_name + "." + file_format)
+    with open(file , 'wb') as file_to_save:
+        file_to_save.write(decoded_b64)
+        file_to_save.close()
 def load_attachment(file_location,file_name):
     with open(file_location + file_name, 'rb') as binary_file:
         attachment = binary_file.read()
@@ -63,8 +64,8 @@ def base64_encode(attachment):
     encoded_b64 = base64.b64encode(attachment)
     return encoded_b64
 def fibep_decode(encoded_json):
-    encoded_json = open(encoded_json,encoding="utf8")
     try:
+        encoded_json = open(encoded_json,encoding="utf8")
         parsed_encoded_json=json.load(encoded_json)
         news = Article(parsed_encoded_json["Provider"],
                        parsed_encoded_json["ItemID"],
@@ -80,26 +81,38 @@ def fibep_decode(encoded_json):
         if "ContentURL" in parsed_encoded_json:
             if parsed_encoded_json["ContentURL"]:
                 news.content_url = parsed_encoded_json["ContentURL"]
-                print("gotcha")
-        print("gotcha")
         if "AdditionalContent" in parsed_encoded_json:
             if parsed_encoded_json["AdditionalContent"]:
-                news.additional_content = parsed_encoded_json["AdditionalContent"]
-        if parsed_encoded_json["ContentURL"]:
-            news.content_url = parsed_encoded_json["ContentURL"] 
-        if parsed_encoded_json["ContractID"]:
-            news.contract_id=parsed_encoded_json["ContractID"] 
-        if parsed_encoded_json["CustomerID"]:
-            news.customer_id=parsed_encoded_json["CustomerID"]
-        if parsed_encoded_json["ProfileID"]:
-            news.profile_id=parsed_encoded_json["ProfileID"]
-        if parsed_encoded_json["Author"]:
-            news.article_author = parsed_encoded_json["Author"]
-        if parsed_encoded_json["Headline"]:
-            news.article_headline=parsed_encoded_json["Headline"]
-        if parsed_encoded_json["MatchingWords"]:
-            news.matching_words=parsed_encoded_json["MatchingWords"]
-        print(vars(news))
+                additional_content = parsed_encoded_json["AdditionalContent"]
+                if additional_content:
+                    for additional_content_element in additional_content:
+                            
+                        if "Base64data" in additional_content_element:
+                            base64data=additional_content_element["Base64data"]
+                            file_format=additional_content_element["Format"]
+                            file_name = additional_content_element["FileName"]
+                            decoded_b64 = base64_decode(base64data)
+                            save_attachment(decoded_b64,file_format,file_location,file_name)
+                            news.additional_content = parsed_encoded_json["AdditionalContent"]
+        if "ContractID" in parsed_encoded_json:
+            if parsed_encoded_json["ContractID"]:
+                news.contract_id=parsed_encoded_json["ContractID"] 
+        if "CustomerID" in parsed_encoded_json:
+            if parsed_encoded_json["CustomerID"]:
+                news.customer_id=parsed_encoded_json["CustomerID"]
+        if "ProfileID" in parsed_encoded_json:
+            if parsed_encoded_json["ProfileID"]:
+                news.profile_id=parsed_encoded_json["ProfileID"]
+        if "Author" in parsed_encoded_json:
+            if parsed_encoded_json["Author"]:
+                news.article_author = parsed_encoded_json["Author"]
+        if "Headline" in parsed_encoded_json:
+            if parsed_encoded_json["Headline"]:
+                news.article_headline=parsed_encoded_json["Headline"]
+        if "MatchingWords" in parsed_encoded_json:
+            if parsed_encoded_json["MatchingWords"]:
+                news.matching_words=parsed_encoded_json["MatchingWords"]
+        #print(vars(news))
         return news
     except Exception as exception:
         logging.critical(f"There's an error decoding your json file, the error is the following: {exception}")
@@ -109,17 +122,14 @@ def fibep_encode(news):
     try:
 
         json.dump(news)
-
-
     except Exception as exception:
         logging.critical(f"There's an error accessing your JSON file, the error is the following: {exception}")
         print("There's no JSON file in the program's folder, please check the logs.")
         sys.exit()
-
 def main():
     try:
         parser = argparse.ArgumentParser(description='This is a simple python script to encode or decode files according to the FIBEP JSON open standard.')
-        parser.add_argument('dir', nargs="?", default=os.getcwd())
+        parser.add_argument('dir', nargs="?", default=file_location)
         parser.add_argument('-e', type=str, help='Encode the file quoted after the e.')
         parser.add_argument('-d', type=str, help='Decode the file quoted after the d.')
         parser.add_argument('-DEBUG', action='store_true', help='Add Debug messages to log.')
@@ -135,14 +145,12 @@ def main():
             fibep_decode(encoded_json)
         if getattr(args,'DEBUG'):
             logging.info("Running with -DEBUG in DEBUG log mode.")
-
     except Exception as exception:
         logging.critical(f"There's an error accessing your JSON file, the error is the following: {exception}")
         print("There's no JSON file in the program's folder, please check the logs.")
         sys.exit()
-
-
 if __name__== '__main__':
         file_location = os.path.dirname(os.path.realpath(__file__))
+        print(file_location)
         main()
 
